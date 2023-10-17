a233a12a5e0c16b247cce70fa7cc9f401e93d896
pfb13 <pfb13@info13.cshl.edu>
2023-10-17 12:04:32 -0400

    first python assignment

---
+++ python1/name_calling.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+print("My name: Benjamin")
+
+print(f"my favorite color: Green")
+
+print("My favorite activity: Listening to cool Music")
+
+print(f"My favorite animal: Hyena")
+
+benjamin=sys.argv[1]
+green=sys.argv[2]
+dancing=sys.argv[3]
+hyena=sys.argv[4]
+
+print('my name is',benjamin,'i love the color',green,'my fav activity',dancing,'my fav animal',hyena)
